5cf2d94953a4b2d167a0b083527d266d8ec5e132
Christopher Lamm <chris@theantichris.com>
2014-05-02 16:15:39 -0400

    Refactored classes to be PSR compliant.

--- src/theantichris/WpPluginFramework/page.php
+++ src/theantichris/WpPluginFramework/page.php
@@ -1,131 +1,131 @@
 <?php
+
+namespace theantichris\WpPluginFramework;
+
 /**
- * An abstract class for creating WordPress pages.
+ * Class Page
  *
- * @author    Christopher Lamm chris@theantichris.com
- * @copyright 2013 Christopher Lamm
- * @license   GNU General Public License, version 3
- * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link      http://www.theantichris.com
+ * A base class for creating and managing WordPress Dashboard pages.
  *
- * @package    WordPress
- * @subpackage WordPressPluginFramework
+ * @package theantichris\WpPluginFramework
  *
- * @since 4.0.0
+ * @since 0.1.0
  */
+abstract class Page
+{
+    /** @var string User readable title for the page and menu item. */
+    protected $pageTitle;
+    /** @var string Unique ID for the page. */
+    protected $pageSlug;
+    /** @var string The capability required for the menu item to be displayed to the user. */
+    protected $capability = 'manage_options';
+    /** @var string|null The URL to the icon to be used for the menu item. */
+    protected $menuIcon = null;
+    /** @var integer|null The position in the menu this page should appear. */
+    protected $position = null;
+    /** @var string The full path to the view file that will display the content. */
+    protected $viewPath;
+    /** @var array Any variables that the templates need access to in an associative array. */
+    protected $viewData = array();
+    /** @var string The WordPress slug for the parent page. */
+    protected $parentSlug;
+    /** @var  string $textDomain Text domain for the plugin. */
+    private $textDomain;
 
-/*
- * TODO: Replace "WordPress_Plugin_Framework" with "Plugin_Name".
- * TODO: Replace "wordpress-plugin-framework" with "plugin-name".
- * TODO: Replace "WordPressPluginFramework" with "PluginName".
- */
+    /**
+     * Class constructor.
+     *
+     * @since 0.1.0
+     *
+     * @param string $pageTitle User readable title for the page and menu item.
+     * @param string $viewPath Path to the view file the page will use to display content.
+     * @param string $capability The capability required for the menu item to be displayed to the user.
+     * @param string|null $menuIcon The URL to the icon to be used for the menu item.
+     * @param string|null $position The position in the menu this page should appear.
+     * @param array $viewData Any variables that the templates need access to in an associative array.
+     * @param array|null $parentSlug The WordPress slug for the parent page.
+     * @param string $textDomain Text domain for the plugin.
+     * @return Page
+     */
+    public function __construct($pageTitle, $viewPath, $capability = null, $menuIcon = null, $position = null,  $viewData = array(), $parentSlug = null, $textDomain = '')
+    {
+        $this->pageTitle = $pageTitle;
+        $this->pageSlug = sanitize_title($pageTitle);
 
-abstract class Page {
-	/** @var string User readable title for the page and menu item. */
-	protected $page_title;
-	/** @var string Unique ID for the page. */
-	protected $page_slug;
-	/** @var string The capability required for the menu item to be displayed to the user. */
-	protected $capability = 'manage_options';
-	/** @var string|null The URL to the icon to be used for the menu item. */
-	protected $icon_url = null;
-	/** @var integer|null The position in the menu this page should appear. */
-	protected $position = null;
-	/** @var string Path to the view file the page will use to display content. */
-	protected $view_path;
-	/** @var array Any variables that the templates need access to in an associative array. */
-	protected $view_data = array();
-	/** @var string The WordPress slug for the parent page. */
-	protected $parent_slug;
+        $this->viewPath = $viewPath;
 
-	/**
-	 * Class constructor.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @param string      $page_title  User readable title for the page and menu item.
-	 * @param string      $view_path   Path to the view file the page will use to display content.
-	 * @param string      $capability  The capability required for the menu item to be displayed to the user.
-	 * @param string|null $icon_url    The URL to the icon to be used for the menu item.
-	 * @param string|null $position    The position in the menu this page should appear.
-	 * @param array       $view_data   Any variables that the templates need access to in an associative array.
-	 * @param array|null  $parent_slug The WordPress slug for the parent page.
-	 *
-	 * @return Page
-	 */
-	public function __construct( $page_title, $view_path, $capability = null, $icon_url = null, $position = null, $view_data = array(), $parent_slug = null ) {
-		$this->page_title = $page_title;
-		$this->page_slug  = WordPress_Plugin_Framework::make_slug( $page_title );
+        if (!empty($capability)) {
+            $this->capability = $capability;
+        }
 
-		$this->view_path = $view_path;
+        if (!empty($menuIcon)) {
+            $this->menuIcon = $menuIcon;
+        }
 
-		if ( !empty( $capability ) ) {
-			$this->capability = $capability;
-		}
+        if (!empty($position)) {
+            $this->position = $position;
+        }
 
-		if ( !empty( $icon_url ) ) {
-			$this->icon_url = $icon_url;
-		}
+        if (!empty($viewData)) {
+            $this->viewData = $viewData;
+        }
 
-		if ( !empty( $position ) ) {
-			$this->position = $position;
-		}
+        $this->viewData['title'] = $this->pageTitle;
+        $this->viewData['slug'] = $this->pageSlug;
 
-		if ( !empty( $view_data ) ) {
-			$this->view_data = $view_data;
-		}
+        $this->parentSlug = $parentSlug;
 
-		$this->view_data[ 'title' ] = $this->page_title;
-		$this->view_data[ 'slug' ] = $this->page_slug;
+        $this->textDomain = $textDomain;
 
-		$this->parent_slug = $parent_slug;
+        add_action('admin_menu', array($this, 'addPage'));
+    }
 
-		add_action( 'admin_menu', array( $this, 'add_page' ) );
-	}
+    /**
+     * Returns the $pageSlug property.
+     *
+     * @since 0.1.0
+     *
+     * @return string
+     */
+    public function getPageSlug()
+    {
+        return $this->pageSlug;
+    }
 
-	/**
-	 * Returns the $page_slug property.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @return string
-	 */
-	public function get_page_slug() {
-		return $this->page_slug;
-	}
+    /**
+     * Adds the page to WordPress.
+     *
+     * @since 0.1.1
+     *
+     * @return void
+     */
+    abstract public function addPage();
 
-	/**
-	 * Adds the page to WordPress.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @return void
-	 */
-	abstract public function add_page();
+    /**
+     * Removes a page from WordPress.
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    public function removePage()
+    {
+        remove_menu_page($this->pageSlug);
+    }
 
-	/**
-	 * Removes a page from WordPress.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @return void
-	 */
-	public function remove_page() {
-		remove_menu_page( $this->page_slug );
-	}
+    /**
+     * Displays the HTML output of the page.
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    public function displayPage()
+    {
+        if (!current_user_can($this->capability)) {
+            wp_die(__('You do not have sufficient permissions to access this page.', $this->textDomain));
+        }
 
-	/**
-	 * Displays the HTML output of the page.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @return void
-	 */
-	public function display_page() {
-		if ( !current_user_can( $this->capability ) ) {
-			wp_die( __( 'You do not have sufficient permissions to access this page.', 'wordpress-plugin-framework' ) );
-		}
-
-		echo View::render( $this->view_path, $this->view_data );
-	}
+        echo View::render($this->viewPath, $this->viewData);
+    }
 }
--- src/theantichris/WpPluginFramework/settings.php
+++ src/theantichris/WpPluginFramework/settings.php
@@ -1,153 +1,167 @@
 <?php
+
+namespace theantichris\WpPluginFramework;
+
+    /*
+     * How settings work in WordPress.
+     *
+     * 1. add_settings_section( $id = 'eg_setting_section', $title = 'Example settings section in reading', $callback = 'eg_setting_section_callback_function', $page = 'reading' )
+     * 2. add_settings_field( $id = 'eg_setting_name', $title = 'Example setting Name', $callback = 'eg_setting_callback_function', $page = 'reading', $section = 'eg_setting_section', $args = array() )
+     * 		$page should equal $page in add_settings_section()
+     * 		$section should equal $id in add_settings_section()
+     * 3. register_setting( $option_group = 'reading', $option_name = 'eg_setting_name', $sanitize_callback = null )
+     * 		$option_group should equal the values for $page in the other functions
+     * 		$option_name should equal $id in add_settings_field()
+     */
+
 /**
- * Class for creating WordPress settings.
+ * Class Settings
  *
- * @author     Christopher Lamm chris@theantichris.com
- * @copyright  2013 Christopher Lamm
- * @license    GNU General Public License, version 3
- * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link       http://www.theantichris.com
+ * A class for creating and managing settings in WordPress.
  *
- * @package    WordPress
- * @subpackage WordPressPluginFramework
+ * @package theantichris\WpPluginFramework
  *
- * @since      5.0.0
+ * @since 0.1.0
  */
+class Settings
+{
+    /** @var string The WordPress page slug the settings will appear on. */
+    private $page = 'general';
+    /** @var mixed[] Information about the settings section if used. */
+    private $section = array(
+        'title' => 'Default',
+        'id' => 'default',
+        'viewPath' => null,
+        'viewData' => array()
+    );
 
-/*
- * TODO: Replace "WordPress_Plugin_Framework" with "Plugin_Name".
- * TODO: Replace "WordPressPluginFramework" with "PluginName".
- */
+    /**
+     * Class constructor.
+     *
+     * @since 0.1.0
+     *
+     * @param string $page
+     */
+    public function __construct($page = 'general')
+    {
+        if ('' != trim($page)) {
+            $this->page = $page;
+        }
+    }
 
-/*
- * Outline
- *
- * 1. add_settings_section( $id = 'eg_setting_section', $title = 'Example settings section in reading', $callback = 'eg_setting_section_callback_function', $page = 'reading' )
- * 2. add_settings_field( $id = 'eg_setting_name', $title = 'Example setting Name', $callback = 'eg_setting_callback_function', $page = 'reading', $section = 'eg_setting_section', $args = array() )
- * 		$page should equal $page in add_settings_section()
- * 		$section should equal $id in add_settings_section()
- * 3. register_setting( $option_group = 'reading', $option_name = 'eg_setting_name', $sanitize_callback = null )
- * 		$option_group should equal the values for $page in the other functions
- * 		$option_name should equal $id in add_settings_field()
- */
+    /**
+     * Adds a settings section to the object.
+     *
+     * @since 0.1.0
+     *
+     * @param string $title User readable title for the settings section.
+     * @param string $viewPath The full path to the settings section's view.
+     * @param array $viewData Any data that needs to be passed to the view.
+     *
+     * @return void
+     */
+    public function addSection($title, $viewPath, $viewData = array())
+    {
+        if (('' != trim($title)) && (file_exists($viewPath))) {
+            $this->section['title'] = $title;
+            $this->section['id'] = sanitize_title($title);
 
-class Settings {
-	/** @var string The WordPress page slug the settings will appear on. */
-	private $page = 'general';
-	/** @var array Information about the settings section if used. */
-	private $section = array(
-		'title'     => 'Default',
-		'id'        => 'default',
-		'view_path' => null,
-		'view_data' => array()
-	);
+            $this->section['viewPath'] = $viewPath;
+            $this->section['viewData'] = $viewData;
+            $this->section['viewData']['title'] = $title;
 
-	/**
-	 * Class constructor.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @param string $page
-	 */
-	public function __construct( $page = 'general' ) {
-		if ( '' != trim( $page ) ) {
-			$this->page = $page;
-		}
-	}
+            add_action('admin_init', array($this, 'registerSection'));
+        }
+    }
 
-	/**
-	 * Adds a settings section to the object.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @param string $title     User readable title for the settings section.
-	 * @param string $view_path Path to the settings section's view.
-	 * @param array  $view_data Any data that needs to be passed to the view.
-	 *
-	 * @return void
-	 */
-	public function add_section( $title, $view_path, $view_data = array() ) {
-		if ( ( '' != trim( $title ) ) && ( file_exists( $view_path ) ) ) {
-			$this->section[ 'title' ] = $title;
-			$this->section[ 'id' ]    = WordPress_Plugin_Framework::make_slug( $title );
+    /**
+     * Registers the settings section with WordPress.
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    public function registerSection()
+    {
+        add_settings_section(
+            $this->section['id'],
+            $this->section['title'],
+            array($this, 'displaySection'),
+            $this->page
+        );
+    }
 
-			$this->section[ 'view_path' ]            = $view_path;
-			$this->section[ 'view_data' ]            = $view_data;
-			$this->section[ 'view_data' ][ 'title' ] = $title;
+    /**
+     * Displays the settings section output.
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    public function displaySection()
+    {
+        echo View::render($this->section['viewPath'], $this->section['viewData']);
+    }
 
-			add_action( 'admin_init', array( $this, 'register_section' ) );
-		}
-	}
+    /**
+     * Adds a settings field to the object.
+     *
+     * @since 0.1.0
+     *
+     * @param string $title User readable name for the field.
+     * @param string $viewPath Path to the view for the field
+     * @param array $viewData Data to pass to the view.
+     * @param array $args Optional arguments the field needs in WordPress.
+     *
+     * @return void
+     */
+    public function addField($title, $viewPath, $viewData = array(), $args = array())
+    {
+        // Make sure both the title and view path are valid.
+        if (('' != trim($title)) && (file_exists($viewPath))) {
+            $page = $this->page;
+            $section = $this->section['id'];
 
-	/**
-	 * Registers the settings section with WordPress.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @return void
-	 */
-	public function register_section() {
-		add_settings_section( $this->section[ 'id' ], $this->section[ 'title' ], array( $this, 'display_section' ), $this->page );
-	}
+            // Call hook to register the setting field with WordPress.
+            add_action(
+                'admin_init',
+                function () use ($title, $viewPath, $viewData, $args, $page, $section) {
+                    $id = sanitize_title($title);
 
-	/**
-	 * Displays the settings section output.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @return void
-	 */
-	public function display_section() {
-		echo View::render( $this->section[ 'view_path' ], $this->section[ 'view_data' ] );
-	}
+                    // Add settings field.
+                    add_settings_field(
+                        $id,
+                        $title,
+                        function () use ($id, $title, $viewPath, $viewData) {
+                            // Display the field's view.
+                            $viewData['title'] = $title;
+                            $viewData['id'] = $id;
+                            echo View::render($viewPath, $viewData);
+                        },
+                        $page,
+                        $section,
+                        $args
+                    );
 
-	/**
-	 * Adds a settings field to the object.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @param string $title     User readable name for the field.
-	 * @param string $view_path Path to the view for the field
-	 * @param array  $view_data Data to pass to the view.
-	 * @param array  $args      Optional arguments the field needs in WordPress.
-	 *
-	 * @return void
-	 */
-	public function add_field( $title, $view_path, $view_data = array(), $args = array() ) {
-		// Make sure both the title and view path are valid.
-		if ( ( '' != trim( $title ) ) && ( file_exists( $view_path ) ) ) {
-			$page    = $this->page;
-			$section = $this->section[ 'id' ];
+                    // Register setting.
+                    register_setting($page, $id);
+                }
+            );
+        }
+    }
 
-			// Call hook to register the setting field with WordPress.
-			add_action( 'admin_init', function () use ( $title, $view_path, $view_data, $args, $page, $section ) {
-				$id = WordPress_Plugin_Framework::make_slug( $title );
-
-				// Add settings field.
-				add_settings_field( $id, $title, function () use ( $id, $title, $view_path, $view_data ) {
-					// Display the field's view.
-					$view_data[ 'title' ] = $title;
-					$view_data[ 'id' ]    = $id;
-					echo View::render( $view_path, $view_data );
-				}, $page, $section, $args );
-
-				// Register setting.
-				register_setting( $page, $id );
-			} );
-		}
-	}
-
-	/**
-	 * Cleanly removes a setting from WordPress.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @param string $page
-	 * @param string $id
-	 *
-	 * @return void
-	 */
-	public function remove_field( $page, $id ) {
-		unregister_setting( $page, $id );
-	}
+    /**
+     * Cleanly removes a setting from WordPress.
+     *
+     * @since 0.1.0
+     *
+     * @param string $page
+     * @param string $id
+     *
+     * @return void
+     */
+    public function removeField($page, $id)
+    {
+        unregister_setting($page, $id);
+    }
 }
