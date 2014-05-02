b9ec851cbe8525092d6017044df8e096906de1a0
Christopher Lamm <chris@theantichris.com>
2014-05-02 14:51:02 -0400

    Namespaced the file.

--- src/theantichris/WpPluginFramework/settings.php
+++ src/theantichris/WpPluginFramework/settings.php
@@ -1,23 +1,6 @@
 <?php
-/**
- * Class for creating WordPress settings.
- *
- * @author     Christopher Lamm chris@theantichris.com
- * @copyright  2013 Christopher Lamm
- * @license    GNU General Public License, version 3
- * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link       http://www.theantichris.com
- *
- * @package    WordPress
- * @subpackage WordPressPluginFramework
- *
- * @since      5.0.0
- */
 
-/*
- * TODO: Replace "WordPress_Plugin_Framework" with "Plugin_Name".
- * TODO: Replace "WordPressPluginFramework" with "PluginName".
- */
+namespace theantichris\WpPluginFramework;
 
 /*
  * Outline
