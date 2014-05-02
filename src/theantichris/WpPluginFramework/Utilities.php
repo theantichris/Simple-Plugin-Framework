11b18b521c165fe53d94a73f6e53f85a13aeea91
Christopher Lamm <chris@theantichris.com>
2014-05-02 13:38:18 -0400

    Code styling.

--- src/theantichris/WpPluginFramework/Utilities.php
+++ src/theantichris/WpPluginFramework/Utilities.php
@@ -11,7 +11,8 @@ namespace theantichris\WpPluginFramework;
  *
  * @since 0.1.0
  */
-class Utilities {
+class Utilities
+{
 
     /**
      * Takes a plural string and returns the singular version.
@@ -24,30 +25,31 @@ class Utilities {
      *
      * @return string
      */
-    public static function makeSingular( $word ) {
+    public static function makeSingular($word)
+    {
         /** @var array[string|bool] $rules Rules for making a string singular. */
         $rules = array(
-            'ss'  => false,
-            'os'  => 'o',
+            'ss' => false,
+            'os' => 'o',
             'ies' => 'y',
             'xes' => 'x',
             'oes' => 'o',
             'ves' => 'f',
-            's'   => ''
+            's' => ''
         );
 
         // Loop through all the rules and do the replacement.
-        foreach ( array_keys( $rules ) as $key ) {
+        foreach (array_keys($rules) as $key) {
             // If the end of the word doesn't match the key, it's not a candidate for replacement. Move on to the next plural ending.
-            if ( substr( $word, ( strlen( $key ) * -1 ) ) != $key )
+            if (substr($word, (strlen($key) * -1)) != $key)
                 continue;
 
             // If the value of the key is false, stop looping and return the original version of the word.
-            if ( $key === false )
+            if ($key === false)
                 return $word;
 
             // We've made it this far, so we can do the replacement.
-            return substr( $word, 0, strlen( $word ) - strlen( $key ) ) . $rules[ $key ];
+            return substr($word, 0, strlen($word) - strlen($key)) . $rules[$key];
         }
 
         return $word;
