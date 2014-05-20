0fab963c0fef38f563df0566f6581b609ddbfe26
Christopher Lamm <chris@theantichris.com>
2014-05-20 19:54:59 -0400

    Created a CustomPostTypeArg class.

---
+++ theantichris/WpPluginFramework/CustomPostTypeArg.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace theantichris\WpPluginFramework;
+
+
+class CustomPostTypeArg
+{
+    /** @var string User-readable plural name of the post type. */
+    public $name;
+    /** @var bool If the post type is publicly accessible by admin and front-end. */
+    public $public;
+    /** @var string[] An array of labels for the post type. */
+    public $labels;
+    /** @var string URL to the post type's menu icon. */
+    public $menuIcon;
+    /** @var string[] The post type's capabilities. */
+    public $capabilities;
+    /** @var string[] What WordPress features the post type supports. */
+    public $supports;
+    /** @var string The text domain for translation. */
+    public $textDomain;
+} 
