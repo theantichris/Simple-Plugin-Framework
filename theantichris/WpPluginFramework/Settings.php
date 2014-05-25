6333e04da4e8bbd7a86399f008587e50e48f0feb
Christopher Lamm <chris@theantichris.com>
2014-05-25 10:24:49 -0400

    Formatting.

--- theantichris/WpPluginFramework/Settings.php
+++ theantichris/WpPluginFramework/Settings.php
@@ -2,18 +2,6 @@
 
 namespace theantichris\WpPluginFramework;
 
-    /*
-     * How settings work in WordPress.
-     *
-     * 1. add_settings_section( $id = 'eg_setting_section', $title = 'Example settings section in reading', $callback = 'eg_setting_section_callback_function', $page = 'reading' )
-     * 2. add_settings_field( $id = 'eg_setting_name', $title = 'Example setting Name', $callback = 'eg_setting_callback_function', $page = 'reading', $section = 'eg_setting_section', $args = array() )
-     * 		$page should equal $page in add_settings_section()
-     * 		$section should equal $id in add_settings_section()
-     * 3. register_setting( $option_group = 'reading', $option_name = 'eg_setting_name', $sanitize_callback = null )
-     * 		$option_group should equal the values for $page in the other functions
-     * 		$option_name should equal $id in add_settings_field()
-     */
-
 /**
  * Class Settings
  *
@@ -39,7 +27,7 @@ class Settings
      */
     public function __construct(SettingsArg $settingsArg)
     {
-        $this->page             = $settingsArg->getPageSlug();
+        $this->page            = $settingsArg->getPageSlug();
         $this->settingsSection = $settingsArg->getSettingsSection();
 
         add_action('admin_init', array($this, 'registerSection'));
