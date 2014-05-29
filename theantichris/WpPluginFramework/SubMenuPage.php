72b41322d536ad7fb08777056fa04484d66de5a3
Christopher Lamm <chris@theantichris.com>
2014-05-29 14:03:53 -0400

    Removed removedPage().

--- theantichris/WpPluginFramework/SubMenuPage.php
+++ theantichris/WpPluginFramework/SubMenuPage.php
@@ -11,26 +11,17 @@ namespace theantichris\WpPluginFramework;
  *
  * @since 0.1.0
  */
-class SubMenuPage extends Page {
-	/**
-	 * Adds the page to WordPress.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @return void
-	 */
-	public function addPage() {
-		add_submenu_page( $this->parentSlug, $this->title, $this->title, $this->capability, $this->slug, array( $this, 'display') );
-	}
-
-	/**
-	 * Removes a page from WordPress.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @return void
-	 */
-	public function removePage() {
-		remove_submenu_page( $this->parentSlug, $this->slug );
-	}
+class SubMenuPage extends Page
+{
+    /**
+     * Adds the page to WordPress.
+     *
+     * @since 0.1.0
+     *
+     * @return void
+     */
+    public function addPage()
+    {
+        add_submenu_page($this->parentSlug, $this->title, $this->title, $this->capability, $this->slug, array($this, 'display'));
+    }
 }
