131f6f03c8ff8f129f2c4060c8cf8a1a53cb7e2d
Christopher Lamm <chris@theantichris.com>
2014-06-24 13:59:46 -0400

    Comment.

--- theantichris/WpPluginFramework/Capability.php
+++ theantichris/WpPluginFramework/Capability.php
@@ -71,6 +71,13 @@ abstract class Capability
     const manage_network_themes  = 'manage_network_themes';
     const manage_network_options = 'manage_network_options';
 
+    /**
+     * Checks if a given value is a valid member of the fake enum.
+     *
+     * @since 3.0.0
+     * @param string $value
+     * @return bool
+     */
     public static function isValid($value)
     {
         return defined("self::{$value}");
