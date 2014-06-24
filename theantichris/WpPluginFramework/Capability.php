9a24305c65bde25cebc590de4fd411a056d5f250
Christopher Lamm <chris@theantichris.com>
2014-06-24 12:25:57 -0400

    No longer abstract.

--- theantichris/WpPluginFramework/Capability.php
+++ theantichris/WpPluginFramework/Capability.php
@@ -11,7 +11,7 @@ namespace theantichris\WpPluginFramework;
  * @package theantichris\WpPluginFramework
  * @since 3.0.0
  */
-abstract class Capability
+class Capability
 {
     const switch_themes          = 'switch_themes';
     const edit_themes            = 'edit_themes';
