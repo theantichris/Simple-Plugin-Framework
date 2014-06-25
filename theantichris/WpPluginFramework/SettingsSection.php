8eb210b904e3c7ac27735c7faf0e076d446e4abb
Christopher Lamm <chris@theantichris.com>
2014-06-25 12:14:03 -0400

    Added addField() to SettingsSection. Added text domain as a property. Reworded error message.

--- theantichris/WpPluginFramework/SettingsSection.php
+++ theantichris/WpPluginFramework/SettingsSection.php
@@ -13,6 +13,10 @@ class SettingsSection
     private $title;
     /** @var View */
     private $view;
+    /** @var string */
+    private $textDomain;
+    /** @var SettingsField[] */
+    private $settingsFields = array();
 
     /**
      * @since 2.0.0
@@ -22,10 +26,12 @@ class SettingsSection
      */
     public function __construct($title, $view, $textDomain = '')
     {
+        $this->textDomain = $textDomain;
+
         if (empty($title)) {
-            wp_die(__('You did not specify a title for your settings section.', $textDomain));
+            wp_die(__('You did not specify a title for your settings section.', $this->textDomain));
         } elseif (empty($view)) {
-            wp_die(__('You did not specify a view for your settings section.', $textDomain));
+            wp_die(__("You did not specify a view for settings section {$title}.", $this->textDomain));
         } else {
             $this->title                   = $title;
             $this->view                    = $view;
@@ -58,5 +64,24 @@ class SettingsSection
     public function display()
     {
         $this->view->render();
+    }
+
+    /**
+     * Adds a SettingsField to this SettingsSection if it does not already exist.
+     *
+     * @since 3.0.0
+     *
+     * @param SettingsField $field
+     * @return $this
+     */
+    public function addField(SettingsField $field)
+    {
+        if (array_key_exists($field->getId(), $this->settingsFields)) {
+            wp_die(__("A section with ID {$field->getId()} was already added to the settings section {$this->getId()} page.", $this->textDomain));
+        } else {
+            $this->settingsFields[$field->getId()] = $field;
+        }
+
+        return $this;
     }
 } 
