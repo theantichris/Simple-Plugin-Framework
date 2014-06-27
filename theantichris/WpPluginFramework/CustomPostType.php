861d2752d9bf6fa46fa4587b157bc30df762eaeb
Christopher Lamm <chris@theantichris.com>
2014-06-27 14:23:32 -0400

    Fixed argument keys.

--- theantichris/WpPluginFramework/CustomPostType.php
+++ theantichris/WpPluginFramework/CustomPostType.php
@@ -205,7 +205,8 @@ class CustomPostType
         return array(
             'labels'       => $this->labels,
             'public'       => $this->public,
-            'menuIcon'     => $this->menuIcon,
+            'menu-position' => $this->menuPosition,
+            'menu-icon'     => $this->menuIcon,
             'capabilities' => $this->capabilities,
             'supports'     => $this->supports,
         );
