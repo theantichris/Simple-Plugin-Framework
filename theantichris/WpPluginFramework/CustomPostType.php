bcefb0ef201adf8cfa12604cc0bc97ffa11bbb5e
Christopher Lamm <chris@theantichris.com>
2014-06-27 12:03:35 -0400

    Added setLabels().

--- theantichris/WpPluginFramework/CustomPostType.php
+++ theantichris/WpPluginFramework/CustomPostType.php
@@ -47,9 +47,9 @@ class CustomPostType
             wp_die(__('You did not specify a name for your custom post type.', $this->textDomain));
         } else {
             $this->name = $name;
+            $this->labels = $this->setLabels();
         }
 
-//        $this->labels       = $customPostTypeArgs->getLabels();
 //        $this->menuIcon     = $customPostTypeArgs->menuIcon;
 //        $this->capabilities = $customPostTypeArgs->capabilities;
 //        $this->supports     = $customPostTypeArgs->supports;
@@ -57,6 +57,41 @@ class CustomPostType
 //        $this->arguments = $this->setArguments();
 //
 //        add_action('init', array($this, 'registerCustomPostType'));
+    }
+
+    /**
+     * Setups the labels for the post type..
+     *
+     * @since 3.0.0
+     *
+     * @return string[]
+     */
+    private function setLabels()
+    {
+        /** @var string $singular Singular version of $name. */
+        $singular = Utilities::makeSingular($this->name);
+
+        /** @var string[] $labels */
+        $labels = array(
+            'name'               => __($this->name, $this->textDomain),
+            'singular_name'      => __($singular, $this->textDomain),
+            'add_new'            => __('Add New', $this->textDomain),
+            'add_new_item'       => __('Add New ' . $singular, $this->textDomain),
+            'edit_item'          => __('Edit ' . $singular, $this->textDomain),
+            'new_item'           => __('New ' . $singular, $this->textDomain),
+            'all_items'          => __('All ' . $this->name, $this->textDomain),
+            'view_item'          => __('View ' . $singular, $this->textDomain),
+            'search_items'       => __('Search ' . $this->name, $this->textDomain),
+            'not_found'          => __('No ' . strtolower($this->name) . ' found.', $this->textDomain),
+            'not_found_in_trash' => __(
+                'No ' . strtolower($this->name) . ' found in Trash.',
+                $this->textDomain
+            ),
+            'parent_item_colon'  => '',
+            'menu_name'          => __($this->name, $this->textDomain)
+        );
+
+        return $labels;
     }
 
     /**
