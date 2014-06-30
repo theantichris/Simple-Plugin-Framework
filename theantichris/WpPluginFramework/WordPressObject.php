1840f267aff9893ed468fdd3d13f563128b81d8b
Christopher Lamm <chris@theantichris.com>
2014-06-30 17:11:49 -0400

    Adding properties and the makeSingular() method from the Utilities class.

--- theantichris/WpPluginFramework/WordPressObject.php
+++ theantichris/WpPluginFramework/WordPressObject.php
@@ -9,5 +9,46 @@ namespace theantichris\WpPluginFramework;
  */
 class WordPressObject
 {
+    protected $textDomain;
+    protected $prefix;
 
+    /**
+     * Takes a plural string and returns the singular version.
+     *
+     * Solution found at https://sites.google.com/site/chrelad/notes-1/pluraltosingularwithphp.
+     *
+     * @since 3.0.0
+     *
+     * @param string $word Plural word to make singular.
+     *
+     * @return string
+     */
+    public static function makeSingular($word)
+    {
+        /** @var mixed[] $rules Rules for making a string singular. The key is the plural ending, the value is the singular ending. */
+        $rules = array(
+            'ss'  => false,
+            'os'  => 'o',
+            'ies' => 'y',
+            'xes' => 'x',
+            'oes' => 'o',
+            'ves' => 'f',
+            's'   => ''
+        );
+
+        /** @var string $key */
+        foreach (array_keys($rules) as $key) {
+            if (substr($word, (strlen($key) * -1)) != $key) {
+                continue;
+            }
+
+            if ($key === false) {
+                return $word;
+            }
+
+            return substr($word, 0, strlen($word) - strlen($key)) . $rules[$key];
+        }
+
+        return $word;
+    }
 }
