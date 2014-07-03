e51bde11fd29b18bea85702a6465194982a4ac9e
Christopher Lamm <chris@theantichris.com>
2014-07-03 09:23:20 -0400

    Properties and constructor.

--- theantichris/WpPluginFramework/DashboardWidget.php
+++ theantichris/WpPluginFramework/DashboardWidget.php
@@ -9,5 +9,27 @@ namespace theantichris\WpPluginFramework;
  */
 class DashboardWidget extends WordPressObject
 {
+    /** @var string The name your widget will display in its heading. */
+    private $name;
+    /** @var View The View object responsible for displaying the widget. */
+    private $view;
 
+    /**
+     * Assigns properties, sets name and slug in the View's $viewData, and ties the addWidget() method to the wp_dashboard_setup hook.
+     * @link http://codex.wordpress.org/Plugin_API/Action_Reference/wp_dashboard_setup
+     *
+     * @since 3.0.0
+     *
+     * @param string $name The name your widget will display in its heading.
+     * @param View $view The View object responsible for displaying the widget.
+     */
+    public function __construct($name, View $view)
+    {
+        $this->name                   = $name;
+        $this->view                   = $view;
+        $this->view->viewData['name'] = $this->name;
+        $this->view->viewData['slug'] = $this->getSlug();
+
+        add_action('wp_dashboard_setup', array($this, 'addWidget'));
+    }
 }
