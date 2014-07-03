dd34d43a3b4ff40f9bf481b96e9e77b2de000c7f
Christopher Lamm <chris@theantichris.com>
2014-07-03 09:16:40 -0400

    Class skeleton.

---
+++ theantichris/WpPluginFramework/DashboardWidget.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace theantichris\WpPluginFramework;
+
+/**
+ * Class DashboardWidget
+ * @package theantichris\WpPluginFramework
+ * @since 3.0.0
+ */
+class DashboardWidget extends WordPressObject
+{
+
+}
