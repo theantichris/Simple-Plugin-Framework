8f36d063beac7582d9c6a7dbf9a662ec16df57c0
Christopher Lamm <chris@theantichris.com>
2014-08-05 09:24:46 -0400

    Moved makeSingular() to a static class.

--- src/WordPressObject.php
+++ src/WordPressObject.php
@@ -19,45 +19,6 @@ abstract class WordPressObject
     protected $viewData;
 
     /**
-     * Takes a plural string and returns the singular version.     *
-     * @link https://sites.google.com/site/chrelad/notes-1/pluraltosingularwithphp
-     *
-     * @since 3.0.0
-     *
-     * @param string $word Plural word to make singular.
-     *
-     * @return string
-     */
-    protected function makeSingular($word)
-    {
-        /** @var mixed[] $rules Rules for making a string singular. The key is the plural ending, the value is the singular ending. */
-        $rules = array(
-            'ss'  => false,
-            'os'  => 'o',
-            'ies' => 'y',
-            'xes' => 'x',
-            'oes' => 'o',
-            'ves' => 'f',
-            's'   => ''
-        );
-
-        /** @var string $key */
-        foreach (array_keys($rules) as $key) {
-            if (substr($word, (strlen($key) * -1)) != $key) {
-                continue;
-            }
-
-            if ($key === false) {
-                return $word;
-            }
-
-            return substr($word, 0, strlen($word) - strlen($key)) . $rules[$key];
-        }
-
-        return $word;
-    }
-
-    /**
      * Returns the object's $name property.
      *
      * @since 3.0.0
