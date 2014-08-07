1ab452cfb3bb62c7c51af3817cfa7b4419415348
Christopher Lamm <chris@theantichris.com>
2014-08-07 15:29:11 -0400

    Updated text input view.

--- src/MetaBoxInputs/TextInput.php
+++ src/MetaBoxInputs/TextInput.php
@@ -3,12 +3,15 @@
 /** @var string $name */
 /** @var string $slug */
 
-/** @var string $fieldName */
-$fieldName = 'spf-meta-' . $slug;
+/** @var string $fieldId */
+$fieldId = 'spf-meta-' . $slug;
 
 /** @var string $value The current value of the custom field. */
 $value = esc_attr(get_post_meta(get_post()->ID, $slug, true));
 
 ?>
 
-<input type="text" name="<?= $fieldName; ?>" id="<?= $fieldName; ?>" value="<?= $value; ?>"/>
+<div>
+    <label for="<?= $fieldId; ?>"><?= $name; ?>:
+        <input type="text" name="<?= $fieldId; ?>" value="<?= $value; ?>"/></label>
+</div>
